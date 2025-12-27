dbf7ba51e82aa5573388f636cf3139bdb149ad28
Gysev <antohazelenov@yandex.ru>
2025-12-27 16:02:02 +0300

    Task 6

--- src/test/java/ru/mtuci/rbpo2025/Rbpo2025ApplicationTests.java
+++ src/test/java/ru/mtuci/rbpo2025/Rbpo2025ApplicationTests.java
@@ -2,8 +2,10 @@ package ru.mtuci.rbpo2025;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Rbpo2025ApplicationTests {
 
 	@Test
